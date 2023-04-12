78c7fdb8a0255be913983691382973925b760a87
Kenneth Fossen <fossen.kenneth@gmail.com>
2023-04-12 23:20:32 +0200

    Updated

--- part-II/SecureApi/Program.cs
+++ part-II/SecureApi/Program.cs
@@ -1,3 +1,6 @@
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        var instance = "";
+        var tenantId = "";
+        options.Authority = $"{instance}{tenantId}";
+        options.Audience = "ClientId";
+    });
 
 var app = builder.Build();
 
