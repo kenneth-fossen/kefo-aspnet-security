734affe70a4238b16ad5561f5437d94307c090aa
Kenneth Fossen <fossen.kenneth@gmail.com>
2023-04-13 21:41:56 +0200

    FLAGHANDLER

--- part-II/SecureApi/Requirements/HasFlagHandler.cs
+++ part-II/SecureApi/Requirements/HasFlagHandler.cs
@@ -4,10 +4,27 @@ namespace SecureApi.Requirements;
 
 public class HasFlagHandler : AuthorizationHandler<HasFlag>
 {
+    private readonly ILogger<HasFlagHandler> _logger;
+    public HasFlagHandler(ILogger<HasFlagHandler> logger)
+    {
+        _logger = logger;
+    }
     protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasFlag requirement)
     {
-        if (false)
+        _logger.LogInformation("Handling Flag");
+        var scope = context
+            .User
+            .FindFirst(claim => claim.Type == "scope");
+
+        if (scope is null)
         {
+            _logger.LogError("Failed to find the correct scope");
+            return Task.CompletedTask;
+        }
+
+        if (scope.Value == requirement.FlagName)
+        {
+            _logger.LogInformation("Success");
             context.Succeed(requirement);
         }
 
